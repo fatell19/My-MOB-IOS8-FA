dc975b0ad50f0a97156b94719d262e1a00848e34
fatell19 <farellanos_londonos@hotmail.com>
2016-02-09 20:13:00 -0500

    Adding Homework 1 & 1-2

---
+++ Assignments/MyFirstApp/MyFirstApp/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Francisco Arellanos on 2/9/16.
+//  Copyright © 2016 Francisco Arellanos. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
